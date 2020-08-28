09215456c99841db65a75b24ab068e4a36e86130
Emilio Silva Schlenker <e18r@disroot.org>
2020-08-28 11:49:54 -0500

    get all entries for all events

--- pig.js
+++ pig.js
@@ -1,7 +1,12 @@
 const dbModule = require('./db.js');
 const Web3 = require('web3');
+const CoinosisV0 = require('./CoinosisV0.abi.json');
+const CoinosisV1 = require('./Coinosis.abi.json');
 const ProxyEvent = require('./ProxyEvent.abi.json');
 const fetch = require('node-fetch');
+const fs = require('fs');
+const parse = require('csv-parse/lib/sync');
+const stringify = require('csv-stringify/lib/sync');
 
 let db, web3xDAI, web3ETH;
 const initialize = async () => {
@@ -12,6 +17,16 @@ const initialize = async () => {
   const infuraKey = 'f81483dac9a84ecda3861bf2d19d693c';
   web3ETH = new Web3(`${infuraURL}/${infuraKey}`);
 }
+const noContractPath = './noContract.csv';
+const BCenLasA1 = 'Introducción a la blockchain para artistas';
+const BCenLasA2 = 'La música en la cadena de bloques';
+const time2event = {
+  '1587666616': null,
+  '1587684996': BCenLasA1,
+  '1588288813': BCenLasA2,
+};
+const addressV0 = '0x3E824EEeD4Ad41a7dFb91F11bf8D39D0C6077B4f';
+const addressV1 = '0xBA77007e5745f7e6F80F78904c7628e30909F828';
 const halfDay = 60 * 60 * 12;
 const apiEndpoint = 'https://min-api.cryptocompare.com/data/v2/histoday';
 const apiOptions = '?fsym=ETH&tsym=USD&limit=1&aggregate=1';
@@ -29,29 +44,100 @@ const formatDate = timestamp => {
   return isoish;
 }
 
-const main = async () => {
-  await initialize();
+const getUserName = async address => {
+  const userInfo = await db.users.findOne({ address });
+  if (!userInfo) return '';
+  const userName = userInfo.name;
+  return userName;
+}
+
+const ethPrices = {};
+const getETHPriceUSD = async timestamp => {
+  if (timestamp in ethPrices) return ethPrices[timestamp];
+  const maxTimestamp = BigInt(timestamp) + BigInt(halfDay);
+  const apiRequest = `${apiURL}&toTs=${maxTimestamp}`;
+  const response = await fetch(apiRequest);
+  const info = await response.json();
+  const data = info.Data.Data;
+  const ethPriceUSD = data[data.length - 1].open;
+  ethPrices[timestamp] = ethPriceUSD;
+  return ethPriceUSD;
+}
+
+const getNoContractEntries = async () => {
+  const stream = fs.readFileSync(noContractPath, { encoding: 'utf-8', });
+  const data = parse(stream, { delimiter: ',', columns: true, });
+  console.log(data);
+  return data;
+}
+
+const getEntriesPreV2 = async (version) => {
   const entries = [];
-  const events = await db.events.find({ version: 1 }).toArray();
+  let hash2url;
+  if (version === 1) {
+    const events = await db.events.find({ version: 1 }).toArray();
+    hash2url = events.reduce((hash2url, event) => {
+      const { url } = event;
+      const hash = web3ETH.utils.sha3(url);
+      hash2url[hash] = url;
+      return hash2url;
+    }, {});
+  }
+  let instance;
+  if (version === 1) {
+    instance = new web3ETH.eth.Contract(CoinosisV1, addressV1);
+  } else {
+    instance = new web3ETH.eth.Contract(CoinosisV0, addressV0);
+  }
+  const assessments = await instance.getPastEvents('Assessment', {
+    fromBlock: 0,
+  });
+  const blockInfo = assessments.reduce((blockInfo, assessment) => {
+    const { blockHash, returnValues } = assessment;
+    const { timestamp, eventURL } = returnValues;
+    const url = version === 1
+          ? hash2url[eventURL]
+          : time2event[timestamp];
+    blockInfo[blockHash] = { url, timestamp };
+    return blockInfo;
+  }, {});
+  const transfers = await instance.getPastEvents('Transfer', { fromBlock: 0, });
+  for (const transfer of transfers) {
+    const { blockHash, returnValues } = transfer;
+    const { url: event, timestamp } = blockInfo[blockHash];
+    if (event === null) continue;
+    const { addr: address, reward: rewardWei } = returnValues;
+    const user = await getUserName(address);
+    const reward = web3ETH.utils.fromWei(rewardWei);
+    const currencyPriceUSD = await getETHPriceUSD(timestamp);
+    const entry = {
+      contractVersion: String(version),
+      event,
+      date: formatDate(timestamp),
+      user,
+      address,
+      reward,
+      currency: 'ETH',
+      currencyPriceUSD: String(currencyPriceUSD),
+      rewardUSD: String(reward * currencyPriceUSD),
+    };
+    console.log(entry);
+    entries.push(entry);
+  }
+  return entries;
+}
+
+const getEntriesV2 = async () => {
+  const entries = [];
+  const events = await db.events.find({ version: 2 }).toArray();
   for (const event of events) {
-    let address;
-    const { currency, version } = event;
-    if (version === 1) {
-      address = '0xBA77007e5745f7e6F80F78904c7628e30909F828';
-    } else {
-      address = event.address;
-    }
+    const { address, currency } = event;
     let instance;
     if (currency === 'ETH') {
       instance = new web3ETH.eth.Contract(ProxyEvent, address);
     } else {
       instance = new web3xDAI.eth.Contract(ProxyEvent, address);
     }
-    // para eventos versión 1
-    // const subscription = instance.events.Transfer(
-    //   { fromBlock: 0 },
-    // );
-    // no sé cómo llegar de la subscripción a los eventos
     const transfers = await instance.getPastEvents('Transfer', {
       fromBlock: 0,
     });
@@ -65,9 +151,9 @@ const main = async () => {
         block = await web3xDAI.eth.getBlock(transfer.blockHash);
       }
       const { timestamp } = block;
-      const userInfo = await db.users.findOne({ address });
-      const user = userInfo && userInfo.name;
+      const user = await getUserName(address);
       const entry = {
+        contractVersion: '2',
         event: event.url,
         date: formatDate(timestamp),
         user,
@@ -76,22 +162,35 @@ const main = async () => {
         currency,
       };
       if (currency === 'ETH') {
-        const maxTimestamp = BigInt(timestamp) + BigInt(halfDay);
-        const apiRequest = `${apiURL}&toTs=${maxTimestamp}`;
-        const response = await fetch(apiRequest);
-        const info = await response.json();
-        const data = info.Data.Data;
-        entry.currencyPriceUSD = data[data.length - 1].open;
-        entry.rewardUSD = reward * entry.currencyPriceUSD;
+        const currencyPriceUSD = await getETHPriceUSD(timestamp);
+        entry.currencyPriceUSD = String(currencyPriceUSD);
+        entry.rewardUSD = String(reward * entry.currencyPriceUSD);
       } else {
-        entry.currencyPriceUSD = 1;
+        entry.currencyPriceUSD = '1.00';
         entry.rewardUSD = reward;
       }
       console.log(entry);
       entries.push(entry);
     }
   }
-  await dbModule.disconnect();
+  return entries;
 }
 
-main();
+const getEntries = async () => {
+  await initialize();
+  const noContractEntries = await getNoContractEntries();
+  const entriesV0 = await getEntriesPreV2(0);
+  const entriesV1 = await getEntriesPreV2(1);
+  const entriesV2 = await getEntriesV2();
+  const entries = [
+    ...noContractEntries,
+    ...entriesV0,
+    ...entriesV1,
+    ...entriesV2,
+  ];
+  const csv = stringify(entries);
+  console.log(csv);
+  dbModule.disconnect();
+}
+
+getEntries();
